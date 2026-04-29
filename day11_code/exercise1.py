442034b476627020206d3c32ad68b63cf3cb6f15
Zephyr <edmendzhang214@gmail.com>
2026-04-29 10:22:05 +0800

    day11 code exercise1

---
+++ day11_code/exercise1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    @Author  : Zephyr
+    @Date    : 2026/4/29 8:32
+    @Project  : PythonProject
+    @File     : exercise1.py
+    @IDE      : PyCharm
+    @Description: 
+"""
